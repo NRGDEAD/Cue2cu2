4b2d8e0a65c7edd67daa3c8bb8c215094a6f6e04
Daniel Bender <dbender@me.com>
2019-05-26 19:45:44 +0200

    Instead of reading the cue sheet multiple times, we now do that once and copy it to a variable. Also added an EOL for non-compatibility mode.

--- cue2cu2.py
+++ cue2cu2.py
@@ -23,30 +23,6 @@
 import os
 import argparse
 
-# argparse - parsing arguments - this seemed to be easier in zsh
-parser = argparse.ArgumentParser(description="Cue2cu2 converts a cue sheet to CU2 format.")
-parser.add_argument("--nocompat", action="store_true", help="Disables compatibility mode")
-parser.add_argument("--compat", action="store_true",  help="Enables compatibility mode (default)")
-parser.add_argument("--stdout", action="store_true",  help="Output to stdout instead of a CU2 file matching the binary image file")
-parser.add_argument("cuesheet")
-args = parser.parse_args()
-compatibility_mode = bool(True)
-if args.nocompat:
-	compatibility_mode = bool(False)
-if args.compat:
-	compatibility_mode = bool(True)
-if args.stdout:
-	stdout = bool(True)
-else:
-	stdout = bool(False)
-
-cuesheet = args.cuesheet
-cuesheet_file = open(cuesheet,"r") # We will close this later
-for line in cuesheet_file:
-	if "FILE" in line and "BINARY" in line:
-		binaryfile = str(line)[6:][::-1][9:][::-1]
-		break
-
 # Function to convert timecode/index position to sector count
 def convert_timecode_to_sectors(timecode):
 	minutes = int(timecode[0:2])
@@ -78,16 +54,61 @@ def timecode_addition(timecode, offset):
 def timecode_substraction(timecode, offset):
 	return convert_sectors_to_timecode(convert_timecode_to_sectors(timecode)-convert_timecode_to_sectors(offset))
 
+# Function to throw an error and exit when something went wrong
+def error(message):
+	if message:
+		sys.exit("Cue2cu2 error: "+message)
+	else:
+		sys.exit("Cue2cu2 error.")
+
+# Parsing arguments with argparse
+parser = argparse.ArgumentParser(description="Cue2cu2 converts a cue sheet to CU2 format.")
+parser.add_argument("--nocompat", action="store_true", help="Disables compatibility mode")
+parser.add_argument("--compat", action="store_true",  help="Enables compatibility mode (default)")
+parser.add_argument("--stdout", action="store_true",  help="Output to stdout instead of a CU2 file matching the binary image file")
+parser.add_argument("cuesheet")
+args = parser.parse_args()
+compatibility_mode = bool(True)
+if args.nocompat:
+	compatibility_mode = bool(False)
+if args.compat:
+	compatibility_mode = bool(True)
+if args.stdout:
+	stdout = bool(True)
+else:
+	stdout = bool(False)
+
+cuesheet = args.cuesheet # Make this a little more handy
+
+# Copy the cue sheet into a variable so we don't have to re-read it from disk again.
+with open(cuesheet,"r") as cuesheet_file:
+	cuesheet_content = cuesheet_file.read()
+
+# Check the cue sheet if the image is supposed to be in Mode 2 with 2352 bytes per sector
+for line in cuesheet_content.splitlines():
+	cuesheet_mode_valid = bool(False)
+	if "MODE2/2352" in line:
+		cuesheet_mode_valid = bool(True)
+		break
+if cuesheet_mode_valid == False:
+	error("Cue sheet indicates this image is not in MODE2/2352")
+
+# Extract the filename of the main image or binary file
+for line in cuesheet_content.splitlines():
+	if "FILE" in line and "BINARY" in line:
+		binaryfile = str(line)[6:][::-1][8:][::-1]
+		break
+
 # Now obtain the variables to be used for the output and add them to said output
 
 output = str()
 
 # Get number of tracks from cue sheet
 ntracks = 0
-for line in cuesheet_file:
+for line in cuesheet_content.splitlines():
 	if "TRACK" in line:
 		ntracks += 1
-cuesheet_file.close() # We don't need to read this anymore
+#cuesheet_file.close() # We don't need to read this anymore
 output = output+"ntracks "+str(ntracks)+"\r\n"
 
 # Get the total runtime/size
@@ -105,11 +126,13 @@ output = output+"data1     "+data1+"\r\n"
 
 # Get the tracks lengths
 tracks = [] # Create empty array
-for line in open(cuesheet): # Open the cue sheet and
+for line in cuesheet_content.splitlines():
 	if "INDEX 01" in line: # Look for the lines with INDEX 01, then
 		tracks.append(line) # Add them to the array
+
 for track in range(2, ntracks+1): # Why do I have to +1 this? Python is weird
-	track_position = tracks[track-1][::-1][:9][::-1][:8] # This is akward
+	#track_position = tracks[track-1][::-1][:9][::-1][:9] # This was when accessing the cue sheet file rather than copying it into a variable for performance
+	track_position = tracks[track-1][::-1][:8][::-1][:9] # I have no idea what I'm doing
 	if compatibility_mode == True:
 		track_position = timecode_addition(track_position,"00:02:00")
 	output = output+"track"+str(track).zfill(2)+"   "+track_position+"\r\n"
@@ -120,6 +143,10 @@ if compatibility_mode == True:
 else:
 	track_end = size
 output = output+"\r\ntrk end   "+track_end
+
+
+if compatibility_mode == False:
+	output = output + "\r\n"
 
 if stdout == True:
 	print(output)
